f9977c4098363c221d34e6f3bdc807ae120d8383
Ali Yorulmaz <aliyorulmazdev@gmail.com>
2024-05-24 17:12:01 +0300

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,15 @@
-import {
-  PHASE_DEVELOPMENT_SERVER,
-  PHASE_PRODUCTION_BUILD,
-} from "next/constants.js";
-
-/** @type {import("next").NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
 const nextConfigFunction = async (phase) => {
-  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
-    const withPWA = (await import("next-pwa")).default({
-      dest: "public",
+    const withPWA = (await import('next-pwa')).default({
+      dest: 'public',
+      register: true,
+      skipWaiting: true,
     });
     return withPWA(nextConfig);
-  }
-  return nextConfig;
 };
 
 export default nextConfigFunction;
