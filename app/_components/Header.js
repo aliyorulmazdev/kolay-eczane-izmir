a086355ca1f11415596efc233acaa0b32b00b744
Ali Yorulmaz <aliyorulmazdev@gmail.com>
2024-05-27 10:46:06 +0300

    - Refactor the text in Turkish language for better readability.

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -6,9 +6,8 @@ const Header = () => {
             <div className="container max-w-md space-y-8 px-4 py-12 text-center">
                 <div className="space-y-4">
                     <h1 className="text-4xl font-semibold tracking-tighter text-gray-900 sm:text-4xl md:text-6xl dark:text-primary">
-                        KolayEczane ile,{" "}
                         <span className="bg-gradient-to-r from-gray-900 to-gray-500 bg-clip-text text-transparent dark:from-primary dark:to-gray-500">
-                            ilacın bir adım uzağında.
+                            İlacın bir adım uzağında.
                         </span>
                     </h1>
                     <p className="text-lg text-gray-600 dark:text-gray-400">
